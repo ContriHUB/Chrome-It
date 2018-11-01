ee5d944f14dd124b4a3e30221c851b286ffcfe71
rituparno <rituparnobiswas@gmail.com>
2018-11-01 14:30:46 +0530

    Corrected errors

--- background.js
+++ background.js
@@ -439,9 +439,11 @@ $(document).ready(function(){
 					if((idx = (txt.toLowerCase()).lastIndexOf("Wikipedia".toLowerCase())) !==-1)
 					{
 						Speech("I am searching this on wikipedia.");
-						txt.replace('Wikipedia','');
+						txt=txt.replace('Wikipedia','');
 						if( ((txt.toLowerCase()).lastIndexOf("search".toLowerCase())) !==-1)
-							txt.replace('Search','');
+							txt=txt.replace('Search','');
+						if( ((txt.toLowerCase()).lastIndexOf("on".toLowerCase())) !==-1)
+							txt=txt.replace('on','');
 						chrome.tabs.create({'url':'https://en.wikipedia.org/w/index.php?search='+txt});
 						
 					}
